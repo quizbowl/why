b6a2eb719660a1c7cb5658345299a11301380aaa
Ophir Lifshitz <hangfromthefloor@gmail.com>
2014-04-29 21:05:06 -0400

    Individual PDFs should know their chapter numbers

--- chapter.mk
+++ chapter.mk
@@ -3,9 +3,10 @@ PDFS=$(MDS:.md=.pdf)
 
 all: $(PDFS)
 
-%.pdf: %.md ../../templates/book.template
+%.pdf: %.md ../../templates/book.template ../order.txt
 	$(info Making $@: $^)
 	pandoc $< -o $@ \
 	--template=../../templates/book.template \
 	--chapters \
+	--variable chapter=$(shell grep -n $< ../order.txt | cut -d: -f1) \
 	--latex-engine=lualatex
