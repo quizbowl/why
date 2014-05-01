0cca3b033d4a94790d3af25b5ab278873e9b48ad
Ophir Lifshitz <hangfromthefloor@gmail.com>
2014-04-30 22:00:38 -0400

    No more compiling latex by proxy via pandoc

--- one-chapter.mk
+++ one-chapter.mk
@@ -1,12 +1,20 @@
 MDS=$(wildcard *.md)
 PDFS=$(MDS:.md=.pdf)
 
+.PRECIOUS: %.tex %.figlist
+
 all: $(PDFS)
 
-%.pdf: %.md ../../templates/book.template ../order.txt
-	$(info Making $@: $^)
+%.tex: %.md ../../templates/book.template ../order.txt
 	pandoc $< -o $@ \
 	--template=../../templates/book.template \
 	--chapters \
 	--variable chapter=$(shell grep -n $< ../order.txt | cut -d: -f1) \
 	--latex-engine=lualatex
+
+%.figlist:
+	lualatex $*.tex
+	test -s $*.makefile && make -f $*.makefile
+
+%.pdf: %.tex %.figlist
+	lualatex $<
